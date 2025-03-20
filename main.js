f0c7e95c34784bf16ca1c51f9632b14660ff67c5
worklt <worklt@163.com>
2025-03-20 19:53:06 +0800

    feat: add key enter to play

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-$(window).on('load', function () {
-    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
-    $('#play-btn').on('click', function () {
-        localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
-        window.location.href = './play' + '#' + $('#m3u8-placeholder')[0].value;
+window.addEventListener('load', function () {
+    const input = document.getElementById('m3u8-placeholder');
+    const playButton = document.getElementById('play-btn');
+
+    input.value = localStorage.getItem('m3u8-link') || '';
+
+    document.addEventListener('keydown', function (event) {
+        console.log('Keydown event triggered');
+        if (event.key === 'Enter') {
+            console.log('Enter key pressed');
+            playButton.click();
+        }
+    });
+
+    playButton.addEventListener('click', function () {
+        console.log('Play button clicked');
+        localStorage.setItem('m3u8-link', input.value);
+        window.location.href = './play' + '#' + input.value;
     });
 });
